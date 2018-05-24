ce26dba22c935a04ccc69db2835a321f7b419b41
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2018-05-24 12:50:33 +0100

    Added the first files of the master microcontroller.

---
+++ Firmware/Rgb/interrupts.c
@@ -0,0 +1,87 @@
+#include "cpu.h"
+#include "hwbp_core_types.h"
+#include "app_ios_and_regs.h"
+#include "app_funcs.h"
+#include "hwbp_core.h"
+
+/************************************************************************/
+/* Declare application registers                                        */
+/************************************************************************/
+extern AppRegs app_regs;
+
+/************************************************************************/
+/* Interrupts from Timers                                               */
+/************************************************************************/
+// ISR(TCC0_OVF_vect, ISR_NAKED)
+// ISR(TCD0_OVF_vect, ISR_NAKED)
+// ISR(TCE0_OVF_vect, ISR_NAKED)
+// ISR(TCF0_OVF_vect, ISR_NAKED)
+// 
+// ISR(TCC0_CCA_vect, ISR_NAKED)
+// ISR(TCD0_CCA_vect, ISR_NAKED)
+// ISR(TCE0_CCA_vect, ISR_NAKED)
+// ISR(TCF0_CCA_vect, ISR_NAKED)
+// 
+// ISR(TCD1_OVF_vect, ISR_NAKED)
+// 
+// ISR(TCD1_CCA_vect, ISR_NAKED)
+
+/************************************************************************/ 
+/* DI0                                                                  */
+/************************************************************************/
+ISR(PORTA_INT0_vect, ISR_NAKED)
+{
+	reti();
+}
+
+/************************************************************************/ 
+/* LEDS_READY0                                                          */
+/************************************************************************/
+ISR(PORTB_INT0_vect, ISR_NAKED)
+{   
+   reti();
+}
+
+/************************************************************************/ 
+/* LEDS_READY1                                                          */
+/************************************************************************/
+ISR(PORTC_INT0_vect, ISR_NAKED)
+{  
+   if (read_LEDS_READY1)
+   {
+      if (app_regs.REG_DI0_CONF == GM_DI0_SYNC)
+      {
+         set_UPDATE_LEDS0;
+         set_UPDATE_LEDS1;
+         clr_UPDATE_LEDS0;
+         clr_UPDATE_LEDS1;
+      }
+      
+      if (app_regs.REG_DI0_CONF == GM_DI0_HIGH_RGBS_ON && read_DI0)
+      {  
+         set_UPDATE_LEDS0;
+         set_UPDATE_LEDS1;
+         clr_UPDATE_LEDS0;
+         clr_UPDATE_LEDS1;
+      }
+   }
+   
+	reti();
+}
+
+/************************************************************************/ 
+/* DISABLING_LEDS0                                                      */
+/************************************************************************/
+ISR(PORTB_INT1_vect, ISR_NAKED)
+{
+	reti();
+}
+
+/************************************************************************/ 
+/* DISABLING_LEDS1                                                      */
+/************************************************************************/
+ISR(PORTC_INT1_vect, ISR_NAKED)
+{
+	reti();
+}
+
