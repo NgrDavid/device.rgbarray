cb3d81c3aca7a692ed4a3198785084c51920c270
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2018-05-24 14:27:15 +0100

    Increased reliability on the LEDS_READY interrupts.

--- Firmware/Rgb/interrupts.c
+++ Firmware/Rgb/interrupts.c
@@ -35,36 +35,58 @@ ISR(PORTA_INT0_vect, ISR_NAKED)
 }
 
 /************************************************************************/ 
-/* LEDS_READY0                                                          */
+/* LEDS_READY0 && LEDS_READY1                                           */
 /************************************************************************/
+bool leds_ready0 = false;
+bool leds_ready1 = false;
+
+void leds_are_ready (void)
+{
+   if (app_regs.REG_DI0_CONF == GM_DI0_SYNC)
+   {
+      set_UPDATE_LEDS0;
+      set_UPDATE_LEDS1;
+      clr_UPDATE_LEDS0;
+      clr_UPDATE_LEDS1;
+   }
+      
+   if (app_regs.REG_DI0_CONF == GM_DI0_HIGH_RGBS_ON && read_DI0)
+   {  
+      set_UPDATE_LEDS0;
+      set_UPDATE_LEDS1;
+      clr_UPDATE_LEDS0;
+      clr_UPDATE_LEDS1;
+   }
+}
+
 ISR(PORTB_INT0_vect, ISR_NAKED)
-{   
+{
+   if (read_LEDS_READY0)
+   {
+      leds_ready0 = true;   
+      if (leds_ready0 && leds_ready1)
+      {
+         leds_ready0 = false;
+         leds_ready1 = false;
+         leds_are_ready();
+      }
+   }   
+   
    reti();
 }
 
-/************************************************************************/ 
-/* LEDS_READY1                                                          */
-/************************************************************************/
 ISR(PORTC_INT0_vect, ISR_NAKED)
 {  
    if (read_LEDS_READY1)
    {
-      if (app_regs.REG_DI0_CONF == GM_DI0_SYNC)
+      leds_ready1 = true; 
+      if (leds_ready0 && leds_ready1)
       {
-         set_UPDATE_LEDS0;
-         set_UPDATE_LEDS1;
-         clr_UPDATE_LEDS0;
-         clr_UPDATE_LEDS1;
+         leds_ready0 = false;
+         leds_ready1 = false;
+         leds_are_ready();    
       }
-      
-      if (app_regs.REG_DI0_CONF == GM_DI0_HIGH_RGBS_ON && read_DI0)
-      {  
-         set_UPDATE_LEDS0;
-         set_UPDATE_LEDS1;
-         clr_UPDATE_LEDS0;
-         clr_UPDATE_LEDS1;
-      }
-   }
+   }      
    
 	reti();
 }
